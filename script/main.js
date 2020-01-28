ded5716423950b56286374f4581a209466330777
Kiran Anchule <kiran.anchule@isobar.com>
2020-01-28 15:19:25 +0530

    added fetch data logic and moved data file to data folder

--- script/main.js
+++ script/main.js
@@ -4,13 +4,24 @@
 function fetchData(url) {
 
     fetch(url).
-    then((resp)>{
-        console.log('response handler');
+    then((resp)=>{
+        console.log('response handler', resp);
+        if(resp.status === 200) {
+            return resp.json();
+        }
     }, (fault)=>{
         console.log('Some error occured while fetching data.')
+    }).
+    then((result)=>{
+        console.log('data: ', result);
+        displayProductsList(result.products)
     })
 }
 
-fetchData('./data.json');
+fetchData('./data/data.json');
 
+function displayProductsList(list) {
+   
+
+}
 
